a20f190d6723279537580dead047f86cb73a3f95
d-j-hirst <djhir3@gmail.com>
2023-03-08 19:03:35 +1100

    Update to include 2023nsw and load TAB odds

--- update.py
+++ update.py
@@ -22,12 +22,20 @@ env = environ.Env(
 environ.Env.read_env('.env')
 
 try:
-    with open('previous.json', 'r') as f:
-        data = json.load(f)
+    with open('previous_sb.json', 'r') as f:
+        sb_data = json.load(f)
 except FileNotFoundError:
-    data = {}
+    sb_data = {}
 
-changes = []
+try:
+    with open('previous_tab.json', 'r') as f:
+        tab_data = json.load(f)
+except FileNotFoundError:
+    tab_data = {}
+
+
+sb_changes = []
+tab_changes = []
 
 # urls = {
 #     'NSW': 'https://www.sportsbet.com.au/betting/politics/australian-federal-politics/electorate-betting-nsw-seats-6484922',
@@ -40,9 +48,15 @@ changes = []
 #     'NT': 'https://www.sportsbet.com.au/betting/politics/australian-federal-politics/electorate-betting-nt-seats-6484664',
 # }
 
+# Important: when changing election URL also make sure to rename previous.json so that
+# the new results don't get mixed up with old ones
 urls = {
-    'all': 'https://www.sportsbet.com.au/betting/politics/vic-politics/victorian-election-electorate-betting-6818921'
+    'all': 'https://www.sportsbet.com.au/betting/politics/nsw-politics/nsw-election-electorate-betting-7155024'
 }
+
+tab_url = 'https://www.tab.com.au/sports/betting/Politics/competitions/NSW%20Politics'
+
+print('Loading data from web ...')
 
 options = Options()
 options.add_argument('headless')
@@ -55,8 +69,41 @@ options.add_argument("disable-gpu")
 webdriver_service = Service(f"/home/{env.str('USERNAME')}/chromedriver/stable/chromedriver")
 driver = webdriver.Chrome(service=webdriver_service, options=options)
 
+
+# TAB site
+driver.get(tab_url)
+elements = driver.find_elements(By.CLASS_NAME, 'template-title')
+for element in elements:
+    if element.text == 'Seat Winner':
+        element.click()
+elements = driver.find_elements(By.CLASS_NAME, 'market-propositions')
+for element in elements:
+    text = element.text
+    if 'SeatW' in element.text:
+        seatWnPos = text.find('SeatW')
+        lines = [a.strip().replace(' (','(') for a in text.split('\n')][1:]
+        seat_name = (lines[0].split('(')[1]).split(')')[0]
+        # Messed-up formatting with Winston Hills in 2023nsw, can't be bothered
+        # accomodating it as there is no 3rd-party here, so just skip
+        if seat_name == 'Coalition': continue
+        parties = [a.split('(')[0] for a in lines if '(' in a]
+        prices = [a for a in lines if '(' not in a]
+        for party, price in zip(parties, prices):
+            if seat_name in tab_data and party in tab_data[seat_name]:
+                old_price = tab_data[seat_name][party]
+                if old_price != price:
+                    tab_changes.append(f'Seat {seat_name}: {party} now at price {price} (changed from {old_price})')
+                    tab_data[seat_name][party] = price
+            else:
+                tab_changes.append(f'Seat {seat_name}: new entry {party} at price {price}')
+                if seat_name not in tab_data:
+                    tab_data[seat_name] = {}
+                tab_data[seat_name][party] = price
+
+
+
 for region, url in urls.items():
-    print(f"Loading webpage for {region} ...")
+    if (len(urls) > 1): print(f"Loading webpage for {region} ...")
     driver.get(url)
 
     num_main_markets = len(driver.find_elements(By.CLASS_NAME,
@@ -84,33 +131,73 @@ for region, url in urls.items():
         for outcome in outcomes:
             party = outcome.find_element(By.CLASS_NAME, 'outcomeName_f19a8l1b').text
             price = outcome.find_element(By.CLASS_NAME, 'priceTextSize_frw9zm9').text
-            if seat_name in data and party in data[seat_name]:
-                old_price = data[seat_name][party]
+            if seat_name in sb_data and party in sb_data[seat_name]:
+                old_price = sb_data[seat_name][party]
                 if old_price != price:
-                    changes.append(f'Seat {seat_name}: {party} now at price {price} (changed from {old_price})')
-                    data[seat_name][party] = price
+                    sb_changes.append(f'Seat {seat_name}: {party} now at price {price} (changed from {old_price})')
+                    sb_data[seat_name][party] = price
             else:
-                changes.append(f'Seat {seat_name}: new entry {party} at price {price}')
-                if seat_name not in data:
-                    data[seat_name] = {}
-                data[seat_name][party] = price
+                sb_changes.append(f'Seat {seat_name}: new entry {party} at price {price}')
+                if seat_name not in sb_data:
+                    sb_data[seat_name] = {}
+                sb_data[seat_name][party] = price
 driver.close()
 
 with open('updates.txt', 'w') as f:
-    for change in changes:
+    print('TAB')
+    f.write('TAB\n')
+    for change in tab_changes:
+        print(change)
+        f.write(change)
+    print('Sportsbet')
+    f.write('Sportsbet\n')
+    for change in sb_changes:
         print(change)
         f.write(change)
 
 now = datetime.datetime.now()
 
-archive_name = (f'Update Archives/update_archive_{now.year}-{now.month}'
+sb_archive_name = (f'Update Archives/update_archive_sb_{now.year}-{now.month}'
                 f'-{now.day}-{now.hour}-{now.minute}-{now.second}.dat')
 
-with open(archive_name, 'w') as f:
-    for seat_name, seat_data in data.items():
+tab_archive_name = (f'Update Archives/update_archive_tab_{now.year}-{now.month}'
+                f'-{now.day}-{now.hour}-{now.minute}-{now.second}.dat')
+
+with open(sb_archive_name, 'w') as f:
+    for seat_name, seat_data in sb_data.items():
         f.write(f'#{seat_name}\n')
         for party_name, party_odds in seat_data.items():
             f.write(f'{party_name},{party_odds}\n')
 
-with open('previous.json', 'w') as f:
-    json.dump(data, f)
+with open(tab_archive_name, 'w') as f:
+    for seat_name, seat_data in tab_data.items():
+        f.write(f'#{seat_name}\n')
+        for party_name, party_odds in seat_data.items():
+            f.write(f'{party_name},{party_odds}\n')
+
+with open('previous_tab.json', 'w') as f:
+    json.dump(tab_data, f)
+
+with open('previous_sb.json', 'w') as f:
+    json.dump(sb_data, f)
+
+def interpret_odds(odds):
+    try:
+        return float(odds)
+    except ValueError:
+        return 1000.0
+
+party_leading = {}
+data_items = [a for a in sb_data.items() if a[0] != '']
+for seat_name, seat_data in data_items:
+    seat_data = [a for a in seat_data.items() if a[0] != '']
+    best_party, odds = min(seat_data, key=lambda x: interpret_odds(x[1]))
+    if best_party == "|Coalition": best_party = "Coalition"
+    if sum(a[1] == odds for a in seat_data) > 1:
+        best_party = 'Tie between ' + ', '.join(a[0] for a in seat_data if a[1] == odds)
+    if best_party not in party_leading:
+        party_leading[best_party] = 0
+    party_leading[best_party] += 1
+
+for party, count in party_leading.items():
+    print (f'{party}: {count} seats')
